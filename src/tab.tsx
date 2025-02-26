2350b671be224400fee212f8efa5c6474cee0673
sholvoir <sovar.he@gmail.com>
2025-02-26 15:00:35 -0500

    tab

--- src/tab.tsx
+++ src/tab.tsx
@@ -5,12 +5,15 @@ export default ({class: className, cindex, children, ...rest}: {
     cindex: Signal<number>;
 } & JSX.HTMLAttributes<HTMLElement>): VNode<HTMLElement> => {
     const childs: Array<VNode<HTMLElement>> = Array.isArray(children) ? children : [children];
-    return <article class={`flex flex-col ${className??''}`} {...rest}>
-        <header class="flex gap-2">{childs?.map((child, i) =>
-            <div key={i} class={`min-w-16 cursor-pointer text-center rounded-t-md px-2 ${cindex.value==i?"active":""}`}
-                onClick={()=>cindex.value=i}>{child.props.title??i}
-            </div>
-        )}</header>
-        <section class="grow p-2">{childs[cindex.value]}</section>
+    return <article class={`flex flex-col ${className??''}`} {...rest}> {
+        childs.length ? <>
+            <header class="flex gap-2">{childs?.map((child, i) =>
+                <div key={i} class={`min-w-16 cursor-pointer text-center rounded-t-md px-2 ${cindex.value==i?"active":""}`}
+                    onClick={()=>cindex.value=i}>{child.props.title??i}
+                </div>
+            )}</header>
+            <section class="grow p-2">{childs[cindex.value]}</section>
+        </> : ''
+    }
     </article>;
 }
