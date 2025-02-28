8da3bb81316e0e5006f1fadf53bd1b0b2ee4c380
sholvoir <sovar.he@gmail.com>
2025-02-28 18:40:23 -0500

    0.2.5

--- src/list.tsx
+++ src/list.tsx
@@ -2,10 +2,10 @@ import { JSX, VNode } from "preact";
 import { Signal } from "@preact/signals";
 import { Options } from "./options.ts";
 
-export default ({options, cindex, ...rest}: {
+export default ({ options, cindex, class: className, activeClass }: {
     options: Options
     cindex: Signal<number>
-} & JSX.HTMLAttributes<HTMLDivElement>): VNode<HTMLDivElement> =>
-<div {...rest}>
-    {options.map((option, i) => <div key={i} onClick={()=>cindex.value=i}>{option.label}</div>)}
-</div>
+    activeClass?: string
+} & JSX.HTMLAttributes<HTMLDivElement>): Array<VNode<HTMLDivElement>> =>
+    options.map((option, i) => <div key={i} onClick={() => cindex.value = i}
+        class={`${className ?? ''} ${cindex.value == i ? activeClass : ''}`}>{option.label}</div>)
--- src/tab.tsx
+++ src/tab.tsx
@@ -7,7 +7,7 @@ export default ({ class: className, cindex, children }: {
     const childs: Array<VNode<HTMLElement>> = Array.isArray(children) ? children : [children];
     return childs.length ? <>
         <header class="flex gap-2">{childs?.map((child, i) =>
-            <div key={i} class={`min-w-16 px-2 cursor-pointer text-center rounded-t-md ${cindex.value == i ? className ?? '' : ''}`}
+            <div key={i} class={`min-w-16 px-2 py-1 cursor-pointer text-center rounded-t-md ${cindex.value == i ? className ?? '' : ''}`}
                 onClick={() => cindex.value = i}>{child.props.title ?? i}
             </div>
         )}</header>
